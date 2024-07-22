6d7f4f354a84928f84becf31670fe8422b0d24fa
Zionbledsoe3 <jzion3634@gmail.com>
2024-07-22 16:49:10 +0000

    cal

--- app.js
+++ app.js
@@ -11,4 +11,34 @@ let decimalAdded = false
 
 const operators = ['+', '-', 'x', '/']
 
-function handleKeyPress(e){}
+function handleKeyPress(e){
+    const key = e.target.dataset.key
+    const lastChar = operation[operation.length -1]
+
+    if (key === "="){
+        return
+    }
+    
+    if(key ===  '.' && decimalAdded){
+        return
+    }
+
+    if(operation.indexOf(key) !== -1){
+        decimalAdded = false
+    }
+}
+
+if(operation.length === 0 && key === '_'){
+    operation += key
+    input.innerHTML = operation
+    return
+}
+
+if (operation.length === 0 && operators.indexOf(key)!== -1){
+    input.innerHTML = operation
+}
+
+if (operators.indexOf(lastChar)!== -1 && operators.innerHTMLOf(key) !== -1){
+    input.innerHTML = operation
+    return
+}
